4268c566a1a022d63edecf928e9d5c879467e10c
chinmay <chinmay11.moghe@gmail.com>
2020-08-13 00:26:08 +0530

    debounce input validation function

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const validations = {
     username: /^[A-Za-z]{8,50}$/,
     password: /^(?=(.*[^A-Za-z0-9]){3})(?=(.*[A-Z]){3})(?=(.*\d){3}).+/,
-    email: /^\w+([\.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+    email: /^[A-Z0-9a-z._%+-]+\@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/,
     pancard: /^[A-Z]{5}[0-9]{4}[A-Z]{1}$/,
     passport: /^[A-Z][1-9]\d\d{4}[1-9]$/
 }
@@ -15,7 +15,31 @@ const errorStatuses = {
     passport: "Please enter a valid passport number. Ex.R2210321"
 }
 
-window.addEventListener("input", function (event) {
+/*
+ optimize input validation to delay the validation
+ to avoid stray keypresses.
+*/
+function debounce(func, wait, immediate) {
+    let timeout;
+    return function executedFunc() {
+        let context = this;
+        let args = arguments;
+        let later = function () {
+            timeout = null;
+            if (!immediate) {
+                func.apply(context, args);
+            }
+        }
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) {
+            func.apply(context, args);
+        }
+    }
+}
+
+const delayedValidate = debounce(function validateInput(event) {
     const messageBox = event.target.nextElementSibling.children[0];
     validator = validations[event.target.name];
     let validationStatus = null;
@@ -27,4 +51,6 @@ window.addEventListener("input", function (event) {
     } else {
         messageBox.innerText = "";
     }
-});
+}, 150);
+
+window.addEventListener("input", delayedValidate);
